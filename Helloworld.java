689ca40b774c3bd8aab7ff47c2690fe5f25c9c75
cibinpr <cibinrg09@gmail.com>
2026-02-06 13:36:46 +0530

    Update and rename new program.txt to Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helloprogramvariable, World!");
+        System.out.println("Hello from Docker!");
     }
 }
+
+
 
 
